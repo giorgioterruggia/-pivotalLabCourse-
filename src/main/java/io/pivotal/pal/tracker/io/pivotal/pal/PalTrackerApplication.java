6ee066dc18eec1a89a676bb73d51ea4b39f96f9f
giorgioTerruggia <giorgio.terruggia@gmail.com>
2018-12-17 14:52:18 +0000

    committ all files

---
+++ src/main/java/io/pivotal/pal/tracker/io/pivotal/pal/PalTrackerApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.pal.tracker.io.pivotal.pal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalTrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+
+    }
+}
